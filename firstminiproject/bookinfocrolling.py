4845b7982b8a26b1baf81879b5d4032da388e3de
eunyo <revliss@naver.com>
2024-01-30 15:14:05 +0900

    [UPDATE]최하단까지스크롤 라이브러리화/ 개수를 지정해 필요한만큼 자료를 배열에 담아내는것까지 구현

---
+++ firstminiproject/bookinfocrolling.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import scroll as scrolltobottom
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+action = ActionChains(driver)
+driver.get("https://search.shopping.naver.com/book/home")
+time.sleep(2)
+scrolltobottom.scroll_to_bottom(driver)
+
+driver.find_element(By.PARTIAL_LINK_TEXT, '가정/요리').click()
+time.sleep(3)
+
+new_window = driver.window_handles[-1]
+driver.switch_to.window(new_window)
+
+# 새 창에서 URL을 가져옵니다.
+new_window_url = driver.current_url
+#print("새 창의 URL:", new_window_url)
+
+scrolltobottom.scroll_to_bottom(driver)
+
+for i in range(1,11):
+    onebook = driver.find_elements(By.XPATH, '//*[@id="book_list"]/ul/li['+str(i)+']')
+    for onebookdata in onebook:
+        eachbook = []
+        eachbook = onebookdata.text.split('\n')
+        print(eachbook)
