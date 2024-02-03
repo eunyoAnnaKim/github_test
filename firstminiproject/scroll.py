d08c7efd6d017f2dbecb08d59c0dbbcbcbfb7f83
eunyo <revliss@naver.com>
2024-02-03 15:41:36 +0900

    [update]2024.02.03

--- firstminiproject/scroll.py
+++ firstminiproject/scroll.py
@@ -1,7 +1,7 @@
 import time
 
 def scroll_to_bottom(driver):
-    prev_height = driver.excute_script("return document.body.scrollHeight")
+    prev_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         time.sleep(1)
